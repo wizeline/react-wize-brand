ce663d852814934df1d9974669e5be921671fc86
haufam <haufam@users.noreply.github.com>
2022-03-14 03:45:58 +0000

    deploy: 2b059d0c399c585bd449664f302981a38d57291f

---
+++ cjs/types/components/Loader/Loader.d.ts
@@ -0,0 +1,7 @@
+import React from "react";
+declare type Props = {
+    svg?: boolean;
+    width?: string;
+};
+declare const Loader: React.FC<Props>;
+export default Loader;
---
+++ cjs/types/components/Loader/Loader.styles.d.ts
@@ -0,0 +1,2 @@
+export declare const LoaderStyle: import("styled-components").StyledComponent<"div", any, {}, never>;
+export declare const LoaderImage: import("styled-components").StyledComponent<"img", any, {}, never>;
