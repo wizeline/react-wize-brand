2b059d0c399c585bd449664f302981a38d57291f
haufam <hau.pham@wizeline.com>
2022-03-14 10:39:51 +0700

    add github workflow

--- dist/cjs/types/components/Loader/Loader.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-declare type Props = {
-    svg?: boolean;
-    width?: string;
-};
-declare const Loader: React.FC<Props>;
-export default Loader;
--- dist/cjs/types/components/Loader/Loader.styles.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const LoaderStyle: import("styled-components").StyledComponent<"div", any, {}, never>;
-export declare const LoaderImage: import("styled-components").StyledComponent<"img", any, {}, never>;
